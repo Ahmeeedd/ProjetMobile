e348601cb5f8a2370936a61c6e36c3563cc2f9bf
Ahmeeedd <ahmed.mrabet@esprit.tn>
2021-10-26 22:04:36 +0200

    add enfant activity

---
+++ MyApplication/app/src/main/java/tn/esprit/project/Add_Enfant_Activity.java
@@ -0,0 +1,14 @@
+package tn.esprit.project;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Add_Enfant_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_enfant);
+    }
+}
