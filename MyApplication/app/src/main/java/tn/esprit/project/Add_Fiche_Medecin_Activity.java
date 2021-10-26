4c2c6afa383c9b318b1241ab36c65fe30dc2eba8
ZOUARIoussema <60824835+ZOUARIoussema@users.noreply.github.com>
2021-10-26 21:54:45 +0100

    model and interface fiche médecin

---
+++ MyApplication/app/src/main/java/tn/esprit/project/Add_Fiche_Medecin_Activity.java
@@ -0,0 +1,14 @@
+package tn.esprit.project;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Add_Fiche_Medecin_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_fiche_medecin);
+    }
+}
---
+++ MyApplication/app/src/main/java/tn/esprit/project/models/FicheMedecin.java
@@ -0,0 +1,12 @@
+package tn.esprit.project.models;
+
+public class FicheMedecin {
+
+    private String nom;
+    private String prenom;
+    private String adresse;
+    private int tel;
+    private String email;
+
+
+}
