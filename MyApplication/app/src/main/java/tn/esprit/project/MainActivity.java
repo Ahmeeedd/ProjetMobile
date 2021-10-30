554bb015db3f4677f5cb8da0349424858ba6668f
Ahmeeedd <ahmed.mrabet@esprit.tn>
2021-10-30 15:58:27 +0200

    interface home + l'implémentation du sign in

--- MyApplication/app/src/main/java/tn/esprit/project/MainActivity.java
+++ MyApplication/app/src/main/java/tn/esprit/project/MainActivity.java
@@ -7,32 +7,74 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import tn.esprit.project.DAO.UserDAO;
+import tn.esprit.project.database.MyDataBase;
+import tn.esprit.project.models.Role;
+import tn.esprit.project.models.User;
 
 public class MainActivity extends AppCompatActivity {
     Intent myintent;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Button btn=(Button) findViewById(R.id.btSignIn);
-        Button bt=(Button) findViewById(R.id.btSignUp);
-        EditText et=(EditText) findViewById(R.id.emailinput);
-        EditText psw=(EditText) findViewById(R.id.passwordinput);
+        Button btnsignin=(Button) findViewById(R.id.btSignIn);
+        Button btsignup=(Button) findViewById(R.id.btSignUp);
+        EditText emailtxt=(EditText) findViewById(R.id.emailinput);
+        EditText pwdtxt=(EditText) findViewById(R.id.passwordinput);
 
-        btn.setOnClickListener(new View.OnClickListener() {
+        btnsignin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                myintent=new Intent(MainActivity.this,Welcome.class);
                 //myintent.putExtra("var1",et.getText().toString());
-                startActivity(myintent);
-            }
+                String email = emailtxt.getText().toString();
+                String passsword = pwdtxt.getText().toString();
+
+                if (email.isEmpty() || passsword.isEmpty()) {
+                    Toast.makeText(getApplicationContext(), "Fields are empty !", Toast.LENGTH_SHORT).show();
+                } else {
+                    MyDataBase db = MyDataBase.getDataBase(getApplicationContext());
+
+                    UserDAO userdao = db.userdao();
+                    new Thread(() -> {
+                        User user = userdao.loginUser(email, passsword);
+                        if (user == null) {
+                            runOnUiThread(() ->
+                                    {
+                                        Toast.makeText(getApplicationContext(), "Invalid Credentials!", Toast.LENGTH_SHORT).show();
+
+                                    }
+                            );
+                        } else {
+                     //       if (user.getRole().toString().equals(Role.Parent)) {
+                                startActivity(new Intent(MainActivity.this, Welcome.class));
+                      //      } else {
+                                //Driver Home Page à developper
+                       //     }
+
+
+                        }
+
+                    }).start();
+
+
+                }
+
+
+         }
         });
-        bt.setOnClickListener(new View.OnClickListener() {
+
+        btsignup.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 myintent=new Intent(MainActivity.this,Register.class);
                 startActivity(myintent);
             }
         });
+
     }
+
 }
--- MyApplication/app/src/main/java/tn/esprit/project/Welcome.java
+++ MyApplication/app/src/main/java/tn/esprit/project/Welcome.java
@@ -9,22 +9,27 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
+import tn.esprit.project.models.User;
+
 public class Welcome extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.welcome_activity);
+
+
         Intent myintent=new Intent(this,addInfo.class);
-        TextView tv=(TextView) findViewById(R.id.name);
-        Bundle bdl =getIntent().getExtras();
+     //   TextView tv=(TextView) findViewById(R.id.name);
+       // Bundle bdl =getIntent().getExtras();
       //  tv.setText(bdl.getString("var1"));
-        FloatingActionButton fab=(FloatingActionButton) findViewById(R.id.floatingActionButton);
-        fab.setOnClickListener(new View.OnClickListener() {
+       // FloatingActionButton fab=(FloatingActionButton) findViewById(R.id.floatingActionButton);
+        /*fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 startActivity(myintent);
             }
-        });
+        });*/
     }
 }
